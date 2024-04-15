ec7c4821fe2da101ed7818de6030fb5e78bf2932
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2024-04-14 22:09:32 -0400

    updated resume.jsx with non-updated copy of resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,1 +1,26 @@
 import { Container, Header } from "semantic-ui-react";
+import Resume from '../../public/images/Resume.jpg';
+
+
+const resumePage = () => {
+    return (
+        <Container>
+            <Header as="h1" style={{ fontSize: '100px', marginBottom: '30px' }}>
+                My Resume
+            </Header>
+            <div
+                style={{
+                    Image: `url(${Resume})`,
+                    minHeight: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    
+                </div>
+        </Container>
+
+    )
+}
+
+export default Resume
